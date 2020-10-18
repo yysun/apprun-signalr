94ff1febb24e68912c9acfbbddb96c593ad3ea82
Eric Sun <eric.sun@rpsrealsolutions.com>
2020-10-18 10:34:49 -0400

    ✨ use Hub methods instead of the generic Run method

--- client/src/signalr.ts
+++ client/src/signalr.ts
@@ -1,8 +1,8 @@
 import app from 'apprun';
 import * as signalR from "@microsoft/signalr";
 
-const apiBaseUrl = 'https://apprun-signalr-demo.azurewebsites.net';
-// const apiBaseUrl = '';
+// const apiBaseUrl = 'https://apprun-signalr-demo.azurewebsites.net';
+const apiBaseUrl = '';
 
 const connection = new signalR.HubConnectionBuilder()
   .withUrl(`${apiBaseUrl}/api`)
@@ -13,4 +13,4 @@ connection.start().catch(console.error);
 
 connection.on("apprun:", (event: string, results) => app.run(event, results));
 
-app.on('signalr:', (event, ...args) => connection.send("Run", event, ...args));
+app.on('signalr:', (event, ...args) => connection.send(event, ...args));
--- server/Hub/AppRunHub.cs
+++ server/Hub/AppRunHub.cs
@@ -4,14 +4,11 @@ namespace AppRun.SignalR
 {
     public class AppRunHub : Hub
     {
-        public void Run(string name, int number, int delta)
+        public void Add(int number, int delta)
         {
-            if(name=="add")
-            {
-                var results = Counter.Add(number, delta);
-                Clients.All.SendAsync("apprun:", name, results);
-                //Clients.Client(Context.ConnectionId).SendAsync("run", name, results);
-            }
+            var results = Counter.Add(number, delta);
+            Clients.All.SendAsync("apprun:", "add", results);
+            //Clients.Client(Context.ConnectionId).SendAsync("apprun:", name, results);
         }
     }
 }
